5d63ccc2879a8e4a0588d46bb25ffce2e4a79722
KevHLJob <KevHLJob@github.com>
2023-06-15 10:09:21 -0600

    Componente parametrizado

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from fastapi import FastAPI
-from reactpy import component, html
 from reactpy.backend.fastapi import configure
-
 
 from reactpy import component, html, run
 
 
-#component reactpy
+#componente parametrizado
+def Photo(alt_text, image_id):
+    return html.img(
+        {
+            "src": f"https://picsum.photos/id/{image_id}/500/200",
+            "style": {"width": "50%"},
+            "alt": alt_text,
+        }
+    )
+
+
 @component
-def MyTodoList():
-    return html.div(
-        html.h1("My Todo List"), 
-        html.img({"src": "https://picsum.photos/id/0/500/300"}), 
-        html.ul(html.li("The first thing I need to do is...")),
+def Gallery():
+    return html.section(
+        html.h1("Photo Gallery"),
+        Photo("Landscape", image_id=830),
+        Photo("City", image_id=274),
+        Photo("Puppy", image_id=237)
     )
 
 
 #server
 app = FastAPI()
-configure(app, MyTodoList)
+configure(app, Gallery)
